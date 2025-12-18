515ab36fad9ecc6f3be74323d502a30330cb2246
Jacob Daddario <jacob.d@hey.com>
2025-12-17 20:15:26 -0600

    Added types for the crossword

--- crosswords/crossword.types.ts
+++ crosswords/crossword.types.ts
@@ -1,41 +1,46 @@
+export type CrosswordClueDirection = "across" | "down";
+
+export type CrosswordAnswer = string;
+
+export type CrosswordAnswerLists = {
+  across: CrosswordAnswer[];
+  down: CrosswordAnswer[];
+};
+
+export type CrosswordClueText = string;
+
+export type CrosswordClueLists = {
+  across: CrosswordClueText[];
+  down: CrosswordClueText[];
+};
+
+// Single letter or "." for black
+export type CrosswordGridCell = string;
+
+export type CrosswordGrid = CrosswordGridCell[];
+
+// "0" indicates no number
+export type CrosswordGridNumber = number;
+
+export type CrosswordGridNumbers = CrosswordGridNumber[];
+
 export type CrosswordSize = {
   cols: number;
   rows: number;
 };
 
-export type CrosswordClueDirection = "across" | "down";
-
-export type CrosswordClueText = string;
-
-export type CrosswordClueList = CrosswordClueText[];
-
-export type CrosswordClueLists = {
-  across: CrosswordClueList;
-  down: CrosswordClueList;
-};
-
-export type CrosswordAnswer = string;
-
-export type CrosswordAnswerList = CrosswordAnswer[];
-
-export type CrosswordAnswerLists = {
-  across: CrosswordAnswerList;
-  down: CrosswordAnswerList;
-};
-
-export type CrosswordGridCell = string; // single letter or "." for black
-
-export type CrosswordGrid = CrosswordGridCell[];
-
-export type CrosswordGridNumber = number; // 0 = no clue starts here
-
-export type CrosswordGridNumbers = CrosswordGridNumber[];
-
 export type CrosswordMeta = {
   author: string;
   copyright: string;
-  date: string; // e.g. "12/1/2008"
-  dow: string; // e.g. "Monday"
+  date: string;
+  dow:
+    | "Monday"
+    | "Tuesday"
+    | "Wednesday"
+    | "Thursday"
+    | "Friday"
+    | "Saturday"
+    | "Sunday";
   editor: string;
   publisher: string;
   title: string;
@@ -43,10 +48,11 @@ export type CrosswordMeta = {
 
 // Core structure needed for rendering and filling the puzzle
 export type CrosswordCore = {
-  admin: boolean;
   answers: CrosswordAnswerLists;
   clues: CrosswordClueLists;
   grid: CrosswordGrid;
   gridnums: CrosswordGridNumbers;
   size: CrosswordSize;
-} & CrosswordMeta;
+};
+
+export type Crossword = CrosswordCore & CrosswordMeta;
