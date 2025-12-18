ed7c9301abcee7f6d1b687b4c233628387c622a5
Jacob Daddario <jacob.d@hey.com>
2025-12-18 00:44:06 -0600

    Adjusted the size of the icon

--- components/crossword/Actions.tsx
+++ components/crossword/Actions.tsx
@@ -6,7 +6,7 @@ export function Actions() {
   return (
     <div className="mt-6">
       <button className="flex items-center hover:bg-gray-200 border border-gray-400 outline-none focus:ring focus:ring-gray-300 px-2.5 py-1 rounded">
-        <CheckSquare2Icon className="size-5 mr-1.5 -mt-px" />
+        <CheckSquare2Icon className="size-4 mr-1.5 -mt-px" />
         Check grid
       </button>
     </div>
