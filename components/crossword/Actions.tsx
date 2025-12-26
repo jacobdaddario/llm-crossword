d8dc14d63a772922a8e6d26cec45b20b4d3a8539
Jacob Daddario <jacob.d@hey.com>
2025-12-26 15:05:40 -0600

    Fixed check values invocation

--- components/crossword/Actions.tsx
+++ components/crossword/Actions.tsx
@@ -20,7 +20,7 @@ export function Actions() {
   return (
     <div className="mt-6">
       <button
-        onClick={() => checkPuzzle(gridState, answers, setGridCorrectness)}
+        onClick={() => checkPuzzle(gridState, [], answers, setGridCorrectness)}
         className="flex items-center hover:bg-gray-200 border border-gray-400 outline-none focus:ring focus:ring-gray-300 px-2.5 py-1 rounded"
       >
         <CheckSquare2Icon className="size-4 mr-1.5 -mt-px" />
